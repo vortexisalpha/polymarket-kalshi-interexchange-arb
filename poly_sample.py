7d48de1e6dd880691533b9598d13273e542950f3
vortexisalpha <joshuaboshua250505@gmail.com>
2025-12-22 18:02:54 +0500

    add polymarket write to text file

--- poly_sample.py
+++ poly_sample.py
@@ -3,6 +3,7 @@ import requests
 BASE = "https://gamma-api.polymarket.com/" #?tag_id=100381&limit=1&closed=false"
 
 tags = requests.get(f"{BASE}/tags").json()
+#you can only filter events by tag so get bitcoin tag (i dont think there is a crypto one) and then add that as a param for events
 for tag in tags:
     if tag["label"] == "Bitcoin":
         print(f"label: {tag['label']} | slug: {tag['slug']}")
@@ -22,3 +23,8 @@ print("\n"*3)
 
 for market in crypto_markets:
     print(market["title"])
+
+
+with open("poly_btc_events.txt", "w") as f:
+    for market in crypto_markets:
+        f.write(f"{market['title']}\n")
