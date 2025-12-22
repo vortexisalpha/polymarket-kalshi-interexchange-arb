ff835cca3171987e370eb4afe3d2c657847abd41
vortexisalpha <joshuaboshua250505@gmail.com>
2025-12-22 18:06:52 +0500

    added functionality for kalshi crypto writing to file

--- kalshi_sample.py
+++ kalshi_sample.py
@@ -13,3 +13,6 @@ for series in series_list:
     print(f"Series Title: {series['title']}")
     ticker_to_title[series["ticker"]] = series["title"]
 
+with open("kalshi_crypto_series.txt", "w") as f:
+    for series in series_list:
+       f.writelines(f"{series['title']}\n") 
