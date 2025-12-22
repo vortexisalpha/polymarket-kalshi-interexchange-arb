a5308feaf0c559adf27e1239c5060a8d31c2d20f
vortexisalpha <joshuaboshua250505@gmail.com>
2025-12-22 17:54:33 +0500

    kalshi sample of how to get crypto series data

---
+++ kalshi_sample.py
@@ -0,0 +1,15 @@
+import requests
+
+BASE = "https://api.elections.kalshi.com/trade-api/v2"
+
+series_params = {"limit" : 100, "category" : "Crypto"}
+
+series_data = requests.get(f"{BASE}/series", params=series_params).json()
+series_list = series_data.get("series", [])
+
+ticker_to_title = {}
+
+for series in series_list:
+    print(f"Series Title: {series['title']}")
+    ticker_to_title[series["ticker"]] = series["title"]
+
