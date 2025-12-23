f4c4958e616164e60c3b5ee0fcbc3c13b7d62bb5
vortexisalpha <joshuaboshua250505@gmail.com>
2025-12-23 22:07:31 +0500

    fully working nicely printed arb logic for bitcoin

--- poly_kalshi_data.py
+++ poly_kalshi_data.py
@@ -35,6 +35,8 @@ class PolyExtractor:
         return events['markets']
 
     def print_market(self, market):
+        if not market:
+            return
         outcomes = json.loads(market.get("outcomes"))
         outcome_prices = json.loads(market.get("outcomePrices"))
         question = market.get("question")
@@ -42,6 +44,14 @@ class PolyExtractor:
             print(f"Poly: {question}")
             for outcome, price in zip(outcomes, outcome_prices):
                 print(f"{outcome}: {price}")
+
+    #this assumes yes/no market
+    def get_market_yes_no_price(self,market):
+        if not market:
+            return
+        outcome_prices = json.loads(market.get("outcomePrices"))
+        return outcome_prices[0], outcome_prices[1]
+
 
 
 class KalshiExtractor:
@@ -79,9 +89,66 @@ class KalshiExtractor:
         return markets
 
     def print_market(self, market):
-        print(f"Kalshi: {market['yes_sub_title']}")
+        if not market:
+            return
+        print(f"Kalshi: {market['title']} {market['yes_sub_title']}")
         print(f"yes ask: {market['yes_ask']}")
         print(f"no ask: {market['no_ask']}")
+    
+    def get_market_yes_no_price(self, market):
+        if not market:
+            return
+        
+        return market['yes_ask'], market['no_ask']
+class ArbitragePair:
+    def __init__(self, k_title, k_yes_price, k_no_price, p_title, p_yes_price, p_no_price):
+        self.kalshi_title = k_title
+        self.kalshi_yes_price = float(k_yes_price)/100
+        self.kalshi_no_price = float(k_no_price)/100
+
+        self.poly_title = p_title
+        self.poly_yes_price = float(p_yes_price)
+        self.poly_no_price = float(p_no_price)
+
+        self.arbitrage = "none" #none for no, pk for yes poly and no kalshi, kp for yes kalshi no poly, both for both
+        self.arbitrage = self.check_arb()
+    def check_arb(self):
+        if self.kalshi_no_price + self.poly_yes_price < 1 and self.kalshi_yes_price + self.poly_no_price < 1:
+            if self.kalshi_no_price + self.poly_yes_price < self.kalshi_yes_price + self.poly_no_price:
+                self.arbitrage = "pk"
+            else:
+                self.arbitrage = "kp"
+        elif self.kalshi_no_price + self.poly_yes_price < 1:
+            self.arbitrage = "pk"
+        elif self.poly_no_price + self.kalshi_yes_price < 1:
+            self.arbitrage = "kp"
+        return self.arbitrage
+    
+    def print(self):
+        if self.arbitrage == "none":
+            return
+    
+        print("="*80) 
+        
+        print("\nKALSHI")
+        print(f"  Title: {self.kalshi_title}")
+        print(f"  YES:   {self.kalshi_yes_price:.4f}")
+        print(f"  NO:    {self.kalshi_no_price:.4f}")
+
+        print("\nPOLYMARKET")
+        print(f"  Title: {self.poly_title}")
+        print(f"  YES:   {self.poly_yes_price:.4f}")
+        print(f"  NO:    {self.poly_no_price:.4f}")
+
+        if (self.arbitrage == "kp"):
+            edge = 1 - (self.poly_no_price + self.kalshi_yes_price)
+            print("\nBuy yes on Kalshi, No on Polymarket\n")
+        else:
+            edge = 1 - (self.poly_yes_price + self.kalshi_no_price) 
+            print("\nBuy yes on Polymarket, No on Kalshi\n")
+        print(f"Edge = {edge}\n")
+        print("="*80)
+
 
 def write_to_file(filepath, data):
     with open(filepath, "w") as f:
@@ -112,22 +179,31 @@ if __name__ == "__main__":
     print(f"len of poly markets {len(bitcoin_poly_markets)}")
     print(f"len of kalshi markets {len(bitcoin_kalshi_markets)}")
 
-    matching_pairs = get_matching_pairs(poly_titles_in = bitcoin_poly_markets,kalshi_titles_in = bitcoin_kalshi_markets)
-    '''
+    #matching_pairs = get_matching_pairs(poly_titles_in = bitcoin_poly_markets,kalshi_titles_in = bitcoin_kalshi_markets)
     with open("arb_pairs.json", "r") as f:
         matching_pairs = json.load(f)
-    '''
+
+    pair_list = [] 
+    #print matching arb pairs
     for matching_pair in matching_pairs:
         poly_title = matching_pair['poly_title']
-        poly_market = poly_extractor.title_to_markets[poly_title]
+        poly_market = poly_extractor.title_to_markets.get(poly_title, {})
         kalshi_title = matching_pair['kalshi_title']
-        kalshi_market = kalshi_extractor.title_to_markets[kalshi_title]
+        kalshi_market = kalshi_extractor.title_to_markets.get(kalshi_title, {})
 
-        poly_extractor.print_market(poly_market)
 
-        print("\n")
+        p_yes, p_no = poly_extractor.get_market_yes_no_price(poly_market)
+        k_yes, k_no = kalshi_extractor.get_market_yes_no_price(kalshi_market)
 
-        print(f"Kalshi: {kalshi_title}")
-        kalshi_extractor.print_market(kalshi_market)
+        pair_list.append(ArbitragePair(kalshi_title, k_yes, k_no, poly_title, p_yes, p_no))
+        #poly_extractor.print_market(poly_market)
+        #print("\n")
+        #kalshi_extractor.print_market(kalshi_market)
+        #print("\n"*3)
+    
+    arbitrage_pair_list = [pair for pair in pair_list if pair.arbitrage != "none"]
 
-        print("\n"*3)
+    print('Arb Pairs:')
+    for arb_pair in arbitrage_pair_list:
+        arb_pair.print()
+        
